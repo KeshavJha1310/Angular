8e47e08668a5141df6d6dd048bc97e8836d89f30
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-23 18:05:58 +0530

    UnderStanding Interpolation

--- MyFirstAngularApp/src/app/books/books.component.ts
+++ MyFirstAngularApp/src/app/books/books.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  bookName = 'Clean Code';
+  authorName = 'Robert C Martin'
 
   constructor() { }
 
