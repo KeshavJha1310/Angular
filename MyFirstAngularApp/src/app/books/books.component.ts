ba8cca09748fbb70ec9e5a192436b95ac72e0ffc
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-23 18:26:03 +0530

    Understanding the Intropolation with Images and Muultiple Values

--- MyFirstAngularApp/src/app/books/books.component.ts
+++ MyFirstAngularApp/src/app/books/books.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  bookName = 'Clean Code';
-  authorName = 'Robert C Martin'
+  bookName:string = 'Clean Code';
+  authorName:string = 'Robert C Martin';
+  clean_code_url:string = 'https://m.media-amazon.com/images/I/71T7aD3EOTL._SY522_.jpg'
+  bookName2:string = 'Pragmatic Programmer';
+  authorName2:string = 'David Thomas';
+  clean_code_url2:string = 'https://m.media-amazon.com/images/I/71Nxk9VhSTL._SY522_.jpg'
 
   constructor() { }
 
