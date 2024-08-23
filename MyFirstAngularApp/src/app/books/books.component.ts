d9654b527c23c562b3982875f67966c89ce19460
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-23 18:41:08 +0530

    Understanding Binding over Interpolation

--- MyFirstAngularApp/src/app/books/books.component.ts
+++ MyFirstAngularApp/src/app/books/books.component.ts
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class BooksComponent implements OnInit {
   bookName:string = 'Clean Code';
   authorName:string = 'Robert C Martin';
-  clean_code_url:string = 'https://m.media-amazon.com/images/I/71T7aD3EOTL._SY522_.jpg'
+  clean_code_url:string = 'https://m.media-amazon.com/images/I/71T7aD3EOTL._SY522_.jpg';
   bookName2:string = 'Pragmatic Programmer';
   authorName2:string = 'David Thomas';
-  clean_code_url2:string = 'https://m.media-amazon.com/images/I/71Nxk9VhSTL._SY522_.jpg'
+  clean_code_url2:string = 'https://m.media-amazon.com/images/I/71Nxk9VhSTL._SY522_.jpg';
+  isDisable:boolean= false
 
   constructor() { }
 
