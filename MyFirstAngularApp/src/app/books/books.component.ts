b5da4c5f3c68a54a1c307bcd25db04a176f862df
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-26 18:02:01 +0530

    ngFor directive

--- MyFirstAngularApp/src/app/books/books.component.ts
+++ MyFirstAngularApp/src/app/books/books.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Book{
+bookName :string;
+authorName:string;
+clean_code_url : string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  bookName:string = 'Clean Code';
-  authorName:string = 'Robert C Martin';
-  clean_code_url:string = 'https://m.media-amazon.com/images/I/71T7aD3EOTL._SY522_.jpg';
-  bookName2:string = 'Pragmatic Programmer';
-  authorName2:string = 'David Thomas';
-  clean_code_url2:string = 'https://m.media-amazon.com/images/I/71Nxk9VhSTL._SY522_.jpg';
+books:Book[]= [
+  {
+    bookName :'Clean Code',
+    authorName : 'Robert C Martin',
+    clean_code_url : 'https://m.media-amazon.com/images/I/71T7aD3EOTL._SY522_.jpg'
+  },
+  {
+    bookName:  'Pragmatic Programmer',
+    authorName:'David Thomas',
+    clean_code_url: 'https://m.media-amazon.com/images/I/71Nxk9VhSTL._SY522_.jpg'
+  }
+]
   // isDisable:boolean= false
   showTypes:boolean = false
   showButton:boolean = true
