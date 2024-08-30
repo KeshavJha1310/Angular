b6c9e13d0c96194e0f50197e2657a556d3c09e38
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-30 18:45:28 +0530

    Authentication Form Structure

--- MyFirstAngularApp/src/app/app.module.ts
+++ MyFirstAngularApp/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { BooksModule } from "./books/books-module.module";
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from "./books/app-routing.module";
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+
 
 @NgModule({
-    declarations: [AppComponent, CartComponent],
+    declarations: [AppComponent, CartComponent, LoginComponent, RegisterComponent],
     imports: [BrowserModule,FormsModule ,BooksModule,AppRoutingModule],
     bootstrap: [AppComponent],
 
