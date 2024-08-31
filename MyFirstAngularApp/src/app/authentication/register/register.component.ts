eb41526869be65479ae8974e41d1f699e6075869
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2024-08-31 09:45:08 +0530

    firebase registeration

--- MyFirstAngularApp/src/app/authentication/register/register.component.ts
+++ MyFirstAngularApp/src/app/authentication/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { registerForm } from 'src/app/interfaces/auth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-register',
@@ -14,13 +17,34 @@ form : registerForm = {
   comform_pass:''
 }
 
+passwordMatched:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   submit(){
-    console.log(this.form)
+
+if(this.form.password != this.form.comform_pass){
+  this.passwordMatched = false
+  return;
+}
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+  .then((userCredential) => {
+    console.log(userCredential)
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+    // console.log(this.form)
   }
 
 }
---
+++ MyFirstAngularApp/src/app/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSdrP9j--FCvxZL4wSCTrIip_Ke_3kdkU",
+  authDomain: "angularbookshop-77928.firebaseapp.com",
+  databaseURL: "https://angularbookshop-77928-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "angularbookshop-77928",
+  storageBucket: "angularbookshop-77928.appspot.com",
+  messagingSenderId: "421948672563",
+  appId: "1:421948672563:web:91f249a0afc15e0f96f146"
+};
+
+// Initialize Firebase
